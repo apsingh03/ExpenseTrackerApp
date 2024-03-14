6097d27ed60afd444d2299bc03e8a13a048b9d63
Ajay Pratap Singh <singhajaypratap606@gmail.com>
2024-03-14 16:09:41 +0530

    android app initialization

---
+++ androidApp/App.tsx
@@ -0,0 +1,13 @@
+// import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <>
+      <Text >jjhgjhgjhghjgjhgjhgjhgjh</Text>
+    </>
+  );
+}
+
+export default App;
